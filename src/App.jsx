ea894068640485cdcaea59784a820d1ce11746bf
Jacky Tung <jacky.tung@langlive.com>
2022-04-03 13:49:33 +0800

    feat: eslint import sort

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import React from "react";
 import "./App.css";
-import Home from "./containers/Home/index.jsx";
+
+import React from "react";
+
+import Home from "./containers/Home/index";
 
 function App() {
   return (
