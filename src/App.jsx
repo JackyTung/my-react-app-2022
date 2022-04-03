aad6f5f021585b8e5a8e2b4e57807b1766c73876
Jacky Tung <jacky.tung@langlive.com>
2022-04-03 15:44:29 +0800

    feat: init redux structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import "./App.css";
 
 import React from "react";
+import { Provider } from "react-redux";
 
 import Home from "@/containers/Home/index.jsx";
+import configureStore from "@/redux/rootStore";
+
+const store = configureStore();
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Provider store={store}>
+        <Home />
+      </Provider>
     </div>
   );
 }
